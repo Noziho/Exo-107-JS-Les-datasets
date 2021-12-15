5170db3dfb8540e117c3e47c6f3156be61551c82
Noah2211 <noah.decroix3@gmail.com>
2021-12-15 15:54:51 +0100

    first step done

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 // Elements pour lesquels vous créerez des datasets directement dans le code HTML.
 let p1 = document.getElementById('p1');
-let p2 = document.getElementById('p1');
-let p3 = document.getElementById('p1');
-let p4 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let p3 = document.getElementById('p3');
+let p4 = document.getElementById('p4');
 
 // Elements pour lesquels vous créerez des datasets depuis JavaScript.
-let p5 = document.getElementById('p1');
-let p6 = document.getElementById('p1');
-let p7 = document.getElementById('p1');
-let p8 = document.getElementById('p1');
+let p5 = document.getElementById('p5');
+let p6 = document.getElementById('p6');
+let p7 = document.getElementById('p7');
+let p8 = document.getElementById('p8');
+
+let number = p1.dataset.number;
+p1.innerHTML = (Number.parseInt(number) + 10).toString()
+
+let chaine = p2.dataset.string;
+p2.innerHTML = chaine.length.toString();
+
+let tableau = p3.dataset.liste.split(',');
+for (let elem of tableau) {
+    p3.innerHTML += "<br>" + elem.trim();
+}
+
+let boolean = p4.dataset.boolean;
+boolean = boolean.toLocaleLowerCase() === "true";
+if (boolean) {
+    p4.innerHTML = "la valeur est true";
+}
+
+else {
+    p4.innerHTML = "la valeur est false";
+}
+
+
 
